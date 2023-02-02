12e2bdaa5221e0ac0531c793a6f9f9c4c391cf0d
João Pedro <116277457+Joao11110@users.noreply.github.com>
2023-02-02 17:02:27 -0300

    Update Q1.c

--- Alocação Dinâmica na Linguagem C: Vetores e Matrizes/Q1.c
+++ Alocação Dinâmica na Linguagem C: Vetores e Matrizes/Q1.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+int main(){
+
+    // Declarações de variáveis
+    int tamanho = 10;
+    int *vetor = (int*)malloc(tamanho*sizeof(int));
+
+    // Laço de repetição
+    for (int i = 0; i < tamanho; i++)
+    {
+        (vetor[i] = i);
+        printf("valores do vetor: %d\n", vetor[i]);
+    }
+    
+   // Liberação de memória 
+   free(vetor);
+
+   return 0;
+}
