4f786c3d8a1fa053653a4aad5ebaa5a97b68420e
João Pedro <116277457+Joao11110@users.noreply.github.com>
2023-02-04 12:49:37 -0300

    Create Q2.c

---
+++ Lista 5/Q2.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    // Declarando variáveis 
+    int i, j, num_quest= 0;
+    int a = 1, b = 1, c = 1, alunos = 2;
+    float porcentagem_aprovacao = 0;
+
+    // Quantidade de pessoas do questionário
+    printf("Quantas questoes a prova possui: ");
+    scanf("%d", &num_quest);
+
+    // Alocação de memória dinâmica das variáveis
+    char *gabarito = (char*)malloc(num_quest * sizeof(char));
+    char *respostas = (char*)malloc(num_quest * sizeof(char));
+    int nota[10];
+
+    // Inserindo o gabarito da prova
+    for (i = 0; i < num_quest; i++)
+    {
+        printf("\nDigite o gabarito da %d questao:\n", a++ );
+        scanf(" %c", &gabarito[i]);
+    }
+
+    // Alunos inserindo as respostas da prova
+    for (i = 0; i < alunos; i++)
+    {
+        printf("\nAluno %d:\n", b++ );
+        for (j = 0; j < num_quest; j++)
+    {
+        printf("\nColoque a resposta da %d questao da prova:\n", c++ );
+        scanf(" %c", &respostas[j]);
+        
+    }
+    c = 1;
+    }
+
+    // Verificação das respostas da prova com o gabarito
+    for (i = 0; i < alunos; i++)
+    {
+        for (j = 0; j < num_quest; j++)
+    {
+        if(respostas[j] == gabarito[j])
+        {
+            nota[j]++;
+        }
+    }
+
+    }
+    
+    // Nota da prova do aluno, calculo e amostragem da porcentagem de aprovação do aluno
+    for (i = 0; i < alunos; i++)
+    {
+        for (j = 0; j < num_quest; j++)
+        {
+            porcentagem_aprovacao = porcentagem_aprovacao*100 / nota[j];
+            printf("\nNota do aluno %d: %d\n",c++, nota[j]);
+            printf("Porcentagem de aprovacao: %.0f%%\n", porcentagem_aprovacao );
+        }
+    }
+        
+    // Liberando os espaços das alocações dinâmicas de memória
+    free(gabarito);
+    free(respostas);
+    
+    return 0;
+}
