ea3bf72484c610bc2ac1cb184eef198d48e91cdd
João Pedro <116277457+Joao11110@users.noreply.github.com>
2023-02-19 16:01:06 -0300

    Create Preenche vetor.c

---
+++ Aula Prática/Alocação Dinâmica - Vetores e Matrizes/Preenche vetor.c
@@ -0,0 +1,24 @@
+// Declaração de bibliotecas para funções básicas como, scanf e printf (stdio.h) e para fazer alocação de memória (stdlib.h) respectivamente.
+#include<stdio.h>
+#include<stdlib.h>
+
+// Função principal
+int main(){
+
+    /* Declaração da variável tamanho tipo int que representará
+    o tamanho do vetor a ser declarado dinâmicamente*/
+    int tamanho = 10;
+    int *vetor = (int*)malloc(tamanho*sizeof(int));
+
+    // Laço de repetição para preencher o vetor com seu índice
+    for (int i = 0; i < tamanho; i++)
+    {
+        (vetor[i] = i);
+        printf("valores do vetor: %d\n", vetor[i]);
+    }
+
+    // Liberação de memória da variável vetor
+    free(vetor);
+
+    return 0;
+}
