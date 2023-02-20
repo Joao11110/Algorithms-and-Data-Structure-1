b839f50a7ea87fdd4c0b9760f6660b5464cbfeb5
João Pedro <116277457+Joao11110@users.noreply.github.com>
2023-02-20 14:11:10 -0300

    Create Valores de x, y e p.c

---
+++ Aula Prática/Ponteiros/Valores de x, y e p.c
@@ -0,0 +1,17 @@
+// Questão 1) Quais serão os valores de x, y e p ao final do trecho de código abaixo:
+
+#include<stdio.h>
+
+int main(){  
+    int x, y, *p;
+    y = 0;      // x = 0, y = 0, *p = 0
+    p = &y;     // x = 0, y = 0, *p = 0
+    x = *p;     // x = 0, y = 0, *p = 0
+    x = 4;      // x = 4, y = 4, *p = 0
+    (*p)++;     // x = 4, y = 4, *p = 1
+    --x;        // x = 3, y = 4, *p = 1
+    (*p) += x;  // x = 3, y = 4, *p = 4
+    return 0;
+}
+
+// Valor final: x = 3, y = 4, *p = 4
